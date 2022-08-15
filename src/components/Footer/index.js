8ac17f52ed057a89fa80f76312d04df26c50b6f0
Usman Ghani Qureshi <78032063+UsmanGhaniQureshi@users.noreply.github.com>
2022-08-15 19:06:54 +0500

    footer center

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,10 +2,8 @@ import SocialMediaLinks from "../SocialMediaLinks";
 
 const Footer = () => {
   return (
-    <footer className="flex h-20 bg-slate-800 text-slate-100 items-center justify-center ">
-      <div>
-        <strong>Designed By Usman Ghani</strong>
-      </div>
+    <footer className="flex flex-col gap-3 items-center justify-center h-20 bg-slate-800 text-slate-100 ">
+      <p className="font-bold">Designed By Usman Ghani</p>
       <div>
         <SocialMediaLinks />
       </div>
